f4af27e8265f8e7b072dee1eaf1733140d9d9ad8
Andreas Jacobsen <andreaja@ifi.uio.no>
2010-02-20 23:59:11 +0100

    Remove fringes.

--- settings.el
+++ settings.el
@@ -5,6 +5,7 @@
   ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.32")
  '(ecb-tip-of-the-day nil)
+ '(fringe-mode 0 nil (fringe))
  '(jde-mvn-command "/usr/local/maven2/bin/mvn")
  '(jde-mvn-default-compiler-source "1.4")
  '(jde-mvn-default-compiler-target "1.5")
@@ -12,10 +13,3 @@
  '(jde-mvn-use-server nil)
  '(nxml-child-indent 4)
  '(vc-follow-symlinks nil))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(fringe ((((class color) (background light)) nil))))
-
