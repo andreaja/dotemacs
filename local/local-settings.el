9c2cac54b9513b47e070df92abeed1121f2c32e5
Andreas Jacobsen <andreaja@ifi.uio.no>
2010-05-07 19:25:51 +0200

    Red cursor, more visible.

--- local/local-settings.el
+++ local/local-settings.el
@@ -1,5 +1,5 @@
 (setq backup-directory-alist '(("." . "~/.emacs-backups")))
 (fset 'yes-or-no-p 'y-or-n-p) 
 (setq inhibit-startup-message t)
-
+(set-cursor-color 'red)
 ;(global-set-key "\M-/" 'hippie-expand)
