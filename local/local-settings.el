520f8bede7fd24bf45732fc61373f6f60f6d4939
Andreas Jacobsen <andreaja@ifi.uio.no>
2011-08-04 20:22:37 +0200

    Marmalade package repo.

--- local/local-settings.el
+++ local/local-settings.el
@@ -1,3 +1,7 @@
 (tool-bar-mode 0)
 (setq org-todo-keywords
       '((sequence "TODO" "WAIT" "|" "DONE" "DLGT")))
+
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
