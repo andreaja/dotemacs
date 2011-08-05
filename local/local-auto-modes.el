83c4713c01bdf4cfa5aa22b4f3f0a5a6145768f9
Andreas Jacobsen <andreaja@ifi.uio.no>
2011-08-05 23:33:21 +0200

    Markdown mode stuff.

--- local/local-auto-modes.el
+++ local/local-auto-modes.el
@@ -2,4 +2,5 @@
 (add-to-list 'auto-mode-alist '("\.pkg$" . plsql-mode))
 (add-to-list 'auto-mode-alist '("\.proc$" . plsql-mode))
 (add-to-list 'auto-mode-alist '("\.vws$" . sql-mode))
-(add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode)) 
+(add-to-list 'auto-mode-alist '("\.pp$" . puppet-mode))
+(add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
