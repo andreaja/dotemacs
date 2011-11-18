0d879e52f28e5319e3409bad8a33b94d3da48702
Andreas Jacobsen <andreaja@ifi.uio.no>
2011-11-18 20:20:46 +0100

    redisplay-dontpause seems to make org-mode with large(ish) files work better

--- local/local-settings.el
+++ local/local-settings.el
@@ -6,3 +6,7 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+;; http://www.masteringemacs.org/articles/2011/10/02/improving-performance-emacs-display-engine/
+;; (makes keyboard scrolling in org-mode faster?)
+(setq redisplay-dont-pause 1)
