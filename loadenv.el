a6f5c9ac2724582188caead233694461b0908cab
Andreas Jacobsen <andreaja@ifi.uio.no>
2013-03-22 20:52:58 +0100

    Remove puppet-mode since it's installed as a package now

--- loadenv.el
+++ loadenv.el
@@ -5,9 +5,7 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/other")
 ;(add-to-list 'load-path "~/.emacs.d/site-lisp/io-mode")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/markdown-mode")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/puppet")
 
-(require 'puppet-mode)
 (require 'markdown-mode)
 
 (load "local-settings")
