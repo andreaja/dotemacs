f7d55ee89cc15518d3e9a8b0d6886f254f4e5140
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-04-04 13:15:02 +0200

    Remove some obsoletes

--- elpa/js2-mode-20140114/js2-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "js2-mode" "20140114" "Improved JavaScript editing mode" '((emacs "24.1")) :keywords '("languages" "javascript") :url "https://github.com/mooz/js2-mode/")
--- elpa/yasnippet-0.8.0/yasnippet-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "yasnippet" "0.8.0" "Yet another snippet extension for Emacs." 'nil :url "http://github.com/capitaomorte/yasnippet")
