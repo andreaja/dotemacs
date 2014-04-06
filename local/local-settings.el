79b2f36d2977228270f5a30682f165429ed381d2
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-04-06 23:18:18 +0200

    Correct PATH on MacOSX (hacky, should improve this)

--- local/local-settings.el
+++ local/local-settings.el
@@ -15,3 +15,11 @@
 (yas-global-mode 1)
 
 (setq ns-use-srgb-colorspace t)
+
+;; Correctly set up PATH from bash variables
+(let ((path (shell-command-to-string ". ~/.bashrc; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path 
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
