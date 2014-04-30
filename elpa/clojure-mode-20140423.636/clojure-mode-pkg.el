88042ee2f1fb3353fabbcb78d481926f1749be27
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-04-30 14:35:13 +0200

    Upgrade packages

---
+++ elpa/clojure-mode-20140423.636/clojure-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode" "20140423.636" "Major mode for Clojure code" 'nil :url "http://github.com/clojure-emacs/clojure-mode" :keywords ("languages" "lisp"))
--- elpa/fancy-narrow-20140402.229/fancy-narrow-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fancy-narrow" "20140402.229" "narrow-to-region with more eye candy." 'nil :url "http://github.com/Bruce-Connor/fancy-narrow-region" :keywords ("faces" "convenience"))
