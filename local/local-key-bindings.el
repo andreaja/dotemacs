e612417560e60a5aca734f1e0fce29dd5c3d4063
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-05-27 10:46:25 +0200

    Test out some new keybindings

--- local/local-key-bindings.el
+++ local/local-key-bindings.el
@@ -34,7 +34,13 @@
 
 (js2r-add-keybindings-with-prefix "C-c C-m")
 
+(define-key my-keys-minor-mode-map [(meta n)] 'flycheck-next-error)
+(define-key my-keys-minor-mode-map [(meta p)] 'flycheck-previous-error)
+
+
 (define-key org-mode-map [remap browse-url-at-point] 'org-open-at-point)
 
 (define-key org-mode-map [S-left] (lambda () (interactive) (message "Use org-todo instead")))
 (define-key org-mode-map [S-right] (lambda () (interactive) (message "Use org-todo instead")))
+
+(define-key my-keys-minor-mode-map [(control .) (control t)] 'org-todo)
