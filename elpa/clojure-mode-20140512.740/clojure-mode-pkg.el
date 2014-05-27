ef58aca524412c3ce3846077605d2636aea00722
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-05-27 10:49:36 +0200

    upgrade packages

--- elpa/clojure-mode-20140512.740/clojure-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "clojure-mode" "20140512.740" "Major mode for Clojure code" 'nil :url "http://github.com/clojure-emacs/clojure-mode" :keywords ("languages" "lisp"))
---
+++ elpa/clojure-mode-20140514.810/clojure-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "clojure-mode" "20140514.810" "Major mode for Clojure code" 'nil :url "http://github.com/clojure-emacs/clojure-mode" :keywords ("languages" "lisp"))
