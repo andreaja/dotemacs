ec346b2164b0f38e10cc989d7a359f834be85a5b
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-07-04 15:29:54 +0200

    install rust-mode

---
+++ elpa/rust-mode-20140618.536/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20140618.536" "A major emacs mode for editing Rust source code" 'nil :url "https://github.com/rust-lang/rust" :keywords '("languages"))
