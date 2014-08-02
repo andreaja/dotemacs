0a4bb193647bc196ac6d7ca8fb84a7f0126285b7
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-08-02 21:31:30 +0200

    Remove gitconfig mode

--- elpa/gitconfig-mode-20140605.520/gitconfig-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; gitconfig-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "gitconfig-mode" "gitconfig-mode.el" (21406
-;;;;;;  6304 0 0))
-;;; Generated autoloads from gitconfig-mode.el
-
-(autoload 'gitconfig-mode "gitconfig-mode" "\
-A major mode for editing .gitconfig files.
-
-\(fn)" t nil)
-
-(dolist (pattern '("/\\.gitconfig\\'" "/\\.git/config\\'" "/git/config\\'" "/\\.gitmodules\\'")) (add-to-list 'auto-mode-alist (cons pattern 'gitconfig-mode)))
-
-;;;***
-
-;;;### (autoloads nil nil ("gitconfig-mode-pkg.el") (21406 6304 648198
-;;;;;;  0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; gitconfig-mode-autoloads.el ends here
