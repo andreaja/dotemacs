b7a08f4f54ebd8854125fd16784dcb6cd2922fb3
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-08-28 14:59:11 +0200

    Use frame title to display mode-line information

--- local/local-settings.el
+++ local/local-settings.el
@@ -50,3 +50,10 @@
   (font-lock-add-keywords nil '(("^[ \t;`']*\n" 0 'mundane-line-face prepend))))
 
 (add-hook 'prog-mode-hook 'add-mundane-line-font-lock)
+
+
+(setq frame-title-format '("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
+                           (vc-mode vc-mode)
+                           "  " mode-line-modes mode-line-misc-info mode-line-end-spaces))
+
+(setq-default mode-line-format nil)
