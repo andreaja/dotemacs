0516b1d2cec85b134d3b7f061f764ee397c41e79
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-10-19 10:56:06 +0200

    Upgrade packages

--- elpa/multiple-cursors-20140804.1527/multiple-cursors-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "multiple-cursors" "20140804.1527" "Multiple cursors for Emacs." 'nil)
--- elpa/slime-20140913.730/slime-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "slime" "20140913.730" "Superior Lisp Interaction Mode for Emacs"
-  '((cl-lib "0.5"))
-  :url "https://github.com/slime/slime" :keywords
-  '("languages" "lisp" "slime"))
