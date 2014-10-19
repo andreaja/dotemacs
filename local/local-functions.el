520f7be1055bacf9aca7e00305c4e3226f26131e
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-10-19 12:33:03 +0200

    Exchange point and mark without activating

--- local/local-functions.el
+++ local/local-functions.el
@@ -185,6 +185,14 @@ by using nxml's indentation rules."
   (kill-buffer (current-buffer)))
 
 
+(defun exchange-point-and-mark-no-activate ()
+  "Identical to \\[exchange-point-and-mark] but will not activate the region."
+  (interactive)
+  (exchange-point-and-mark)
+  (deactivate-mark nil))
+(define-key global-map [remap exchange-point-and-mark] 'exchange-point-and-mark-no-activate)
+
+
 (defun end-of-code-or-end-of-next-line ()
   "Move to the end of the line. If already there, move to the end of next line"
   (interactive)
