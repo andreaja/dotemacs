7abffc84508f4c83d4d8006b6dfebeedfa385d58
Andreas Jacobsen <andreaja@ifi.uio.no>
2014-12-13 20:47:27 +0100

    Upgrade packages

--- elpa/ess-20141202.645/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20141202.645" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/hungry-delete-20141207.2125/hungry-delete-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hungry-delete" "20141207.2125" "hungry delete minor mode" 'nil :url "http://github.com/nflath/hungry-delete")
