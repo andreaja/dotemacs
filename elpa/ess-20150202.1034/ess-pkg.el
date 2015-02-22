2e5438e25a2265292f40f73bbdfaabd8cb056e1e
Andreas Jacobsen <andreaja@ifi.uio.no>
2015-02-22 21:13:57 +0100

    Upgrade packages

--- elpa/ess-20150202.1034/ess-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "ess" "20150202.1034" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/ess-20150217.932/ess-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ess" "20150217.932" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
