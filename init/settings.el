c1a5b489c0da99a6479c643125e8fbc1b8b6e1a7
Andreas Jacobsen <andreaja@ifi.uio.no>
2015-05-25 12:20:17 +0200

    Shouldn't ever require manual saves

--- init/settings.el
+++ init/settings.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-interval 5)
+ '(auto-save-timeout 1)
  '(flycheck-disabled-checkers (quote (emacs-lisp-checkdoc)))
  '(fringe-mode 0 nil (fringe))
  '(indent-tabs-mode nil)
