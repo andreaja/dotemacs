4195e6d3ff42d4675c92553a55ee482f8c4a1bd7
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-01-08 21:29:24 +0100

    Update ido configuration

--- local/local-ido.el
+++ local/local-ido.el
@@ -2,15 +2,8 @@
 
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
 
-(setq ido-execute-command-cache nil)
+(setq ido-execute-command-cache nil
+      ido-create-new-buffer 'always
+      ido-use-filename-at-point 'guess)
 (ido-mode t)
 
-;; TODO make this work
-;; (defun aja-ido-find-file-in-tag-files ()
-;;   (interactive)
-;;   (save-excursion
-;;     (let ((enable-recursive-minibuffers t))
-;;       (visit-tags-table-buffer))
-;;       (ido-completing-read "Project files: "
-;;                         (tags-table-files)
-;;                         nil t)))
