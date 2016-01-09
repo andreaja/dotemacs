596e78b0b22146be9b4c903cebbbc2edf7a1e31f
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-01-09 12:12:26 +0100

    Visible mark doesn't quite do what I want

--- elpa/visible-mark-20150623.2150/visible-mark-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "visible-mark" "20150623.2150" "Make marks visible." 'nil :url "https://gitlab.com/iankelling/visible-mark" :keywords '("marking" "color" "faces"))
