802cca8451091545b140beb8d76a4fa8126906e9
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-01-17 20:37:46 +0100

    Rely on uniquify instead of manually renaming pom-files

--- local/local-hooks.el
+++ local/local-hooks.el
@@ -4,16 +4,6 @@
              (or (file-exists-p (file-name-directory buffer-file-name))
                  (make-directory (file-name-directory buffer-file-name) t))))
 
-;; For pom-file buffers
-(defun detect-and-rename-pom-file-buffer ()
-  (interactive)
-  (when (string= (buffer-name) "pom.xml")
-    (let* ((folders (split-string (buffer-file-name) "/"))
-           (base-dir (car (last folders 2))))
-      (rename-buffer (format "%s-pom" base-dir)))))
-
-(add-hook 'find-file-hook 'detect-and-rename-pom-file-buffer)
-
 (require 'super-save)
 
 (defun save-some-buffers-quietly ()
