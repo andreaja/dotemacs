865a2d3cc1a96ec574cf1f19d327be98c6601833
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-02-12 21:03:57 +0100

    Upgrade packages

---
+++ elpa/super-save-20160211.256/super-save-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "super-save" "20160211.256" "Auto-save buffers, based on your activity." '((emacs "24.4")) :url "https://github.com/bbatsov/super-save" :keywords '("convenience"))
