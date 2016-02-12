3d1a2b5694c7e52e28295b0363ef8b50810e5757
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-02-12 21:27:14 +0100

    Some sensible defaults

--- local/local-minor-modes.el
+++ local/local-minor-modes.el
@@ -1,11 +1,7 @@
-(prefer-coding-system 'utf-8)
-
 (setq ruby-insert-encoding-magic-comment nil)
 
 (setq backup-directory-alist '(("." . "~/.emacs-backups")))
 (setq auto-save-default nil)
-
-(fset 'yes-or-no-p 'y-or-n-p)
 
 (setq-default truncate-lines nil)
 
