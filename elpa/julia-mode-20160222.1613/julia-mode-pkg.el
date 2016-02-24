a36273729574893372004a282de2939f953a062d
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-02-24 21:53:46 +0100

    Upgrade packages

---
+++ elpa/julia-mode-20160222.1613/julia-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "julia-mode" "20160222.1613" "Major mode for editing Julia source code" 'nil :url "https://github.com/JuliaLang/julia" :keywords '("languages"))
---
+++ elpa/rust-mode-20160222.2142/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20160222.2142" "A major emacs mode for editing Rust source code" 'nil :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
