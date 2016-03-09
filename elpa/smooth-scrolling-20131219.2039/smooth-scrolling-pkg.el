473eb06ed4f7854d77b4ceb8095d0148f2af073f
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-03-09 19:25:29 +0100

    Upgrade packages

--- elpa/smooth-scrolling-20131219.2039/smooth-scrolling-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smooth-scrolling" "20131219.2039" "Make emacs scroll smoothly" 'nil :url "http://github.com/aspiers/smooth-scrolling/" :keywords '("convenience"))
---
+++ elpa/smooth-scrolling-20160227.1715/smooth-scrolling-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "smooth-scrolling" "20160227.1715" "Make emacs scroll smoothly" 'nil :url "http://github.com/aspiers/smooth-scrolling/" :keywords '("convenience"))
