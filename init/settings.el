382457ff93263a0c71d39bcd1acdf747d90b07bf
Andreas Jacobsen <andreaja@ifi.uio.no>
2016-06-23 14:59:11 +0200

    install elm-mode

--- init/settings.el
+++ init/settings.el
@@ -28,7 +28,7 @@
  '(org-catch-invisible-edits (quote smart))
  '(package-selected-packages
    (quote
-    (gradle-mode smooth-scrolling diminish solarized-theme idomenu htmlize s web-mode smex super-save lua-mode elnode ace-window urlenc smartscan slime rust-mode ruby-refactor restclient rainbow-mode rainbow-delimiters puppet-mode projectile php-mode paredit midje-mode malabar-mode magit lorem-ipsum json-reformat js2-refactor jquery-doc insert-shebang inf-ruby hungry-delete groovy-mode gh ggtags flycheck f expand-region ess clojure-test-mode browse-kill-ring aggressive-indent)))
+    (flycheck-elm elm-mode gradle-mode smooth-scrolling diminish solarized-theme idomenu htmlize s web-mode smex super-save lua-mode elnode ace-window urlenc smartscan slime rust-mode ruby-refactor restclient rainbow-mode rainbow-delimiters puppet-mode projectile php-mode paredit midje-mode malabar-mode magit lorem-ipsum json-reformat js2-refactor jquery-doc insert-shebang inf-ruby hungry-delete groovy-mode gh ggtags flycheck f expand-region ess clojure-test-mode browse-kill-ring aggressive-indent)))
  '(require-final-newline (quote visit-save))
  '(safe-local-variable-values (quote ((js2-basic-offset . 2) (js-indent-level . 2))))
  '(show-paren-delay 0)
