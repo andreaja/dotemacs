ead43fba77feb3a915f550d5ea60860a087b2dfb
Andreas Jacobsen <andreaja@ifi.uio.no>
2017-03-16 12:12:01 +0100

    Upgrade packages

--- elpa/smartscan-20160705.138/smartscan-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smartscan" "20160705.138" "Jumps between other symbols found at point" 'nil :keywords '("extensions"))
--- elpa/smooth-scrolling-20160612.141/smooth-scrolling-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "smooth-scrolling" "20160612.141" "Make emacs scroll smoothly" 'nil :url "http://github.com/aspiers/smooth-scrolling/" :keywords '("convenience"))
