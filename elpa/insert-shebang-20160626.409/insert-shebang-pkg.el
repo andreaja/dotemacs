dc684fdc59cbcfc17facfcdffa62bacb7467e5e5
Andreas Jacobsen <andreaja@ifi.uio.no>
2017-10-12 05:28:37 +0200

    Upgrade packages

--- elpa/insert-shebang-20160626.409/insert-shebang-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "insert-shebang" "20160626.409" "Insert shebang line automatically." 'nil :url "http://github.com/psachin/insert-shebang" :keywords '("shebang" "tool" "convenience"))
