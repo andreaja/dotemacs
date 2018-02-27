34f81eef369b72fe9a7081fab29a29e40de4da3d
Andreas Jacobsen <andreaja@ifi.uio.no>
2018-02-27 08:57:53 +0100

    Upgrade packages

---
+++ elpa/inf-ruby-20180226.1511/inf-ruby-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "inf-ruby" "20180226.1511" "Run a Ruby process in a buffer" 'nil :commit "1a5aebea0d701aaeae3158e6a1d5407d7d341ff1" :url "http://github.com/nonsequitur/inf-ruby" :keywords '("languages" "ruby"))
--- elpa/marshal-20171018.932/marshal-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "marshal" "20171018.932" "eieio extension for automatic (un)marshalling" '((eieio "1.4") (json "1.3") (ht "2.1")) :commit "6a31133b25addabc5ec9c87ba959d69c5122171b" :url "https://github.com/sigma/marshal.el" :keywords '("eieio"))
