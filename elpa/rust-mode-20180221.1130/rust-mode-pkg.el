f7d0afa5a73842deb3ac736dce797311d6b06b6a
Andreas Jacobsen <andreaja@ifi.uio.no>
2018-04-09 20:12:25 +0200

    Upgrade packages

--- elpa/rust-mode-20180221.1130/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20180221.1130" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "cc2435b9e5899c0787929a62dc6e6dcdb982febc" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
