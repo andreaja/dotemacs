287d6ceb0f5302fd770c5e9af8a34b7d18ab0d6a
Andreas Jacobsen <andreaja@ifi.uio.no>
2020-02-29 19:44:39 +0100

    Upgrade packages

--- elpa/git-commit-20190717.29/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "03df8596dcc7945f689528bb4dae6dd50bf7dc40" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
