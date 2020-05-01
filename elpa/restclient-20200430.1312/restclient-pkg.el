8f94aea255205f8e74960ffc1b8b56f8c17bfb48
Andreas Jacobsen <andreaja@ifi.uio.no>
2020-05-01 09:56:12 +0200

    Upgrade packages

---
+++ elpa/restclient-20200430.1312/restclient-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/andreaja/.emacs.d/elpa/restclient-20200430.1312/restclient.el  -*- no-byte-compile: t -*-
+(define-package "restclient" "20200430.1312" "An interactive HTTP client for Emacs" 'nil :commit "e4f1c9824b7bc9b8041709d2914de2850806d59c" :keywords '("http") :authors '(("Pavel Kurnosov" . "pashky@gmail.com")) :maintainer '("Pavel Kurnosov" . "pashky@gmail.com"))
