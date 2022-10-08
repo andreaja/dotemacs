b32b1afa82352231cd55a75d628ef7ac4291cb8c
Andreas Jacobsen <andreaja@ifi.uio.no>
2022-10-08 12:49:04 +0200

    Add command to set frame size to half of macbook air monitor
    Something in the latest version of emacs is preventing hammerspoon
    from resizing the undecorated frame and this was the easiest way to
    work around it

--- local/local-mac-mode.el
+++ local/local-mac-mode.el
@@ -1,3 +1,10 @@
 (setq ns-command-modifier (quote meta))
 (setq mac-option-modifier nil)
 (setq locate-command "mdfind")
+
+(defun andreaja/half-screen ()
+  "set frame size to half the macbook air screen"
+  (interactive)
+  (set-frame-size (selected-frame) 731 915 t))
+
+
