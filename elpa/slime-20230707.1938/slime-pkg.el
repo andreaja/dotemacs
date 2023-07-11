ed1ec435f491788201a67e1cd7a7706fbe6d536f
Andreas Jacobsen <andreaja@ifi.uio.no>
2023-07-11 22:25:26 +0200

    Upgrade packages

---
+++ elpa/slime-20230707.1938/slime-pkg.el
@@ -0,0 +1,9 @@
+(define-package "slime" "20230707.1938" "Superior Lisp Interaction Mode for Emacs"
+  '((cl-lib "0.5")
+    (macrostep "0.9"))
+  :commit "ba3d0794e7b2eb7b539fedd8109904b1fb4512a8" :keywords
+  '("languages" "lisp" "slime")
+  :url "https://github.com/slime/slime")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
