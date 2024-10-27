284a02e02d252fcc8662e895bccb69116141d152
Andreas Jacobsen <andreaja@ifi.uio.no>
2024-10-27 08:53:48 +0100

    Upgrade packages

---
+++ elpa/js2-mode-20240908.1236/js2-mode-pkg.el
@@ -0,0 +1,15 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "js2-mode" "20240908.1236"
+  "Improved JavaScript editing mode."
+  '((emacs  "24.1")
+    (cl-lib "0.5"))
+  :url "https://github.com/mooz/js2-mode"
+  :commit "62d6cb169ed4cb6d7ae3ddbd8da9c995fed0ab32"
+  :revdesc "62d6cb169ed4"
+  :keywords '("languages" "javascript")
+  :authors '(("Steve Yegge" . "steve.yegge@gmail.com")
+             ("mooz" . "stillpedant@gmail.com")
+             ("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainers '(("Steve Yegge" . "steve.yegge@gmail.com")
+                 ("mooz" . "stillpedant@gmail.com")
+                 ("Dmitry Gutov" . "dmitry@gutov.dev")))
---
+++ elpa/slime-20241024.144/slime-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "slime" "20241024.144"
+  "Superior Lisp Interaction Mode for Emacs."
+  '((emacs     "24.3")
+    (macrostep "0.9"))
+  :url "https://github.com/slime/slime"
+  :commit "ee57957cec00af190edbf9483087181ffe88dc53"
+  :revdesc "ee57957cec00"
+  :keywords '("languages" "lisp" "slime"))
