fa7993e19f9da5d51d5426abb75b886dbdc86f97
Andreas Jacobsen <andreaja@ifi.uio.no>
2025-06-15 09:24:10 +0200

    Upgrade packages

---
+++ elpa/projectile-20250612.646/projectile-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "projectile" "20250612.646"
+  "Manage and navigate projects in Emacs easily."
+  '((emacs "26.1"))
+  :url "https://github.com/bbatsov/projectile"
+  :commit "0da59734fbc23fc26222a7d03f6671b3116b0b77"
+  :revdesc "0da59734fbc2"
+  :keywords '("project" "convenience")
+  :authors '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
+  :maintainers '(("Bozhidar Batsov" . "bozhidar@batsov.dev")))
---
+++ elpa/rust-mode-20250609.1420/rust-mode-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "rust-mode" "20250609.1420"
+  "A major-mode for editing Rust source code."
+  '((emacs "25.1"))
+  :url "https://github.com/rust-lang/rust-mode"
+  :commit "0d5d54a19e89f9012f3fa84174f562ab18cebb96"
+  :revdesc "0d5d54a19e89"
+  :keywords '("languages")
+  :authors '(("Mozilla" . "rust-mode@noreply.github.com"))
+  :maintainers '(("Mozilla" . "rust-mode@noreply.github.com")))
