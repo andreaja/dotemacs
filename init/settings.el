4e57b7bf3f56ae3193da89fcc589ee9924913364
Andreas Jacobsen <andreaja@ifi.uio.no>
2026-02-08 11:06:49 +0100

    Install visual-fill-column

--- init/settings.el
+++ init/settings.el
@@ -41,7 +41,8 @@
                 org-bullets paredit prettier-js projectile
                 rainbow-delimiters rainbow-mode restclient rust-mode s
                 slime smartscan smex smooth-scrolling solarized-theme
-                super-save terraform-mode urlenc web-mode))
+                super-save terraform-mode urlenc visual-fill-column
+                web-mode))
  '(require-final-newline 'visit-save)
  '(safe-local-variable-values '((js2-basic-offset . 2) (js-indent-level . 2)))
  '(show-paren-delay 0)
@@ -54,6 +55,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#ec423a" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#ec423a" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#db5823" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#c49619" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#93a61a" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#3cafa5"))))
+ '(org-level-6 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#3c98e0"))))
+ '(org-level-7 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#7a7ed2"))))
+ '(org-level-8 ((t (:inherit default :weight bold :font "Lucida Grande" :foreground "#e2468f"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "Purple"))))
  '(rainbow-delimiters-depth-2-face ((t (:foreground "#8F0427"))))
  '(rainbow-delimiters-depth-3-face ((t (:foreground "#C74E6C"))))
