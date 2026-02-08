7d1f05b9fbc99f3d67fc8472a17adb3d5b2b6d83
Andreas Jacobsen <andreaja@ifi.uio.no>
2026-02-08 11:14:33 +0100

    Use visual-fill-mode to wrap before frame end

--- local/local-minor-modes.el
+++ local/local-minor-modes.el
@@ -8,3 +8,11 @@
 (require 'hl-tags-mode)
 
 (add-hook 'org-mode-hook 'visual-line-mode)
+
+;; Configure visual-fill-column for org-mode
+;; Wraps text at 120 characters instead of window width
+(use-package visual-fill-column
+  :config
+  (setq-default visual-fill-column-width 80)
+  (setq-default visual-fill-column-center-text nil)
+  :hook (org-mode . visual-fill-column-mode))
