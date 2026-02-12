bb62c31e02ee483bb797d7e959c3d619dec6e8d3
Andreas Jacobsen <andreaja@ifi.uio.no>
2026-02-12 07:29:57 +0100

    Silence error messages from auto-revert on calendar

--- local/local-desktop.el
+++ local/local-desktop.el
@@ -2,9 +2,11 @@
   (mapcar (lambda (x) (find-file-noselect x)) list-of-files)
   )
 
+
 (defun load-calendar ()
   (interactive)
   (find-file (expand-file-name CALENDAR_FILE))
+  (setq-local auto-revert-use-notify nil)
   (auto-revert-mode))
 
 (find-files (read-lines (expand-file-name "~/.org.file.list")))
